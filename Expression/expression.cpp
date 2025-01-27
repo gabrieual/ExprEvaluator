c27f98784b449ceddc2e56b677eb80f587faa6c3
Gabriel <gabrielalmeidamaciel194@gmail.com>
2025-01-27 14:45:22 -0600

    Expression abstract class

---
+++ Expression/expression.cpp
@@ -0,0 +1,22 @@
+#include "expression.h"
+#include "../Literal/literal.h"
+#include <stdexcept>
+
+namespace ExpressionEvaluator
+{
+    // Static factory method to create expressions from tokens
+    Expression*
+    Expression::createExpression(const std::string &token){
+        try{ // First try to create an bool literal
+            return new BoolLiteral(token);
+        }
+        catch (const char*){
+            try{ // If not an boolean, try int literal
+                return new IntLiteral(token);
+            }
+            catch (const char*){ // If neither, throw an error
+                throw "Unkown literal";
+            }
+        }
+    }
+}
---
+++ Expression/expression.h
@@ -0,0 +1,24 @@
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+#include <stdexcept>
+
+namespace ExpressionEvaluator {
+    class Expression { // Abstract base class for all expressions
+    public:
+        virtual ~Expression() = default;
+        
+        // Pure virtual method to evaluate the expression
+        virtual bool isBoolean() const = 0;
+        virtual int evaluateInt() const = 0;
+        virtual bool evaluateBool() const = 0;
+        
+        // Factory method for creating expressions
+        static Expression* createExpression(const std::string& token);
+
+        virtual Expression* clone() const = 0; // Virtual clone method
+    };
+}
+
+#endif
+
